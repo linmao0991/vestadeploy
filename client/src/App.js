e27ba8621bfc731c065e9bf90c48dfe2a89f5dfa
linmao0991 <lin.maoxian0991@gmail.com>
2020-12-06 08:03:22 -0500

    Attempting to removed react dev tools #2

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import Vesta from "./pages/Vesta";
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 function App() {
   return (
